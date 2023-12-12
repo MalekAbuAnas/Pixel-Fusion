e79010ce2dff518a1b4f08fd8e3deb963ae70b61
Abu Anas <alsaieddesign@gmail.com>
2023-12-12 15:30:28 +0300

    Back-End

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './mongodb/connect';
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import pixelFusionRoutes from './routes/pixelFusionRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit: "50mb"}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/pixel_fusion', pixelFusionRoutes);
 
 const PORT = 3001;
 
